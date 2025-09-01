e7212b1fc73f9c4742bd42ab15b69d1885376164
Melih Can <melihcankilic@gmail.com>
2025-09-01 22:38:24 +0300

    a
    a

--- src/pages/AkisPage.vue
+++ src/pages/AkisPage.vue
@@ -3,6 +3,22 @@
     <h1>Akış Oluşturma Sayfası</h1>
     <q-btn>Akış Oluştur</q-btn>
   </div>
+
+  <div class="q-pa-md" style="max-width: 300px">
+    <div class="q-gutter-md">
+      <q-select
+        standout
+        v-model="modelAdimTipi"
+        :options="optionsAdimTipi"
+        label="Adım Tipi"
+      />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const modelAdimTipi = ref(null);
+const optionsAdimTipi = ['Kişi', 'Rol', 'Birim', 'Üst Birim', 'Generic'];
+</script>
