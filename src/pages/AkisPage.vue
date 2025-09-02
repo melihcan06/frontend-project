1a20ee5d5d69b502aab031d0a2b743c4b962ff05
Melih Can <melihcankilic@gmail.com>
2025-09-02 21:28:19 +0300

    a
    a

--- src/pages/AkisPage.vue
+++ src/pages/AkisPage.vue
@@ -8,32 +8,74 @@
     <div class="q-gutter-md">
       <q-select
         standout
-        v-model="modelAkisAdimTip"
+        v-model="akisAdimTip"
         :options="optionsAkisAdimTip"
         label="Adım Tipi"
       />
-      <!-- <q-input v-model="modelKisiHavuzu" type="text" label="Kişi Havuzu" /> -->
+      <!-- <q-input v-model="kisiHavuzu" type="text" label="Kişi Havuzu" /> -->
       <q-select
         standout
-        v-model="modelAkisAdimHedefTip"
+        v-model="akisAdimHedefTip"
         :options="optionsAkisAdimHedefTip"
         label="Adım Hedef Tipi"
       />
     </div>
+  </div>
+  <!-- <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+      <q-date v-model="date" minimal today-btn />
+    </div>
+  </div> -->
+
+  <div class="q-pa-md">
+    <div class="q-mb-sm">
+      <q-badge color="teal"> Tarih: {{ adimHedefTipTarih }} </q-badge>
+    </div>
+
+    <q-btn icon="event" round color="primary">
+      <q-popup-proxy
+        @before-show="updateProxyAdimHedefTipTarih"
+        cover
+        transition-show="scale"
+        transition-hide="scale"
+      >
+        <q-date v-model="proxyAdimHedefTipTarih">
+          <div class="row items-center justify-end q-gutter-sm">
+            <q-btn label="Cancel" color="primary" flat v-close-popup />
+            <q-btn
+              label="OK"
+              color="primary"
+              flat
+              @click="selectAdimHedefTipTarih"
+              v-close-popup
+            />
+          </div>
+        </q-date>
+      </q-popup-proxy>
+    </q-btn>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-// const modelKisiHavuzu = ref(null);
-const modelAkisAdimTip = ref(null);
+// const kisiHavuzu = ref(null);
+const akisAdimTip = ref(null);
 const optionsAkisAdimTip = ['Kişi', 'Rol', 'Birim'];
-const modelAkisAdimHedefTip = ref(null);
+const akisAdimHedefTip = ref(null);
 const optionsAkisAdimHedefTip = [
   'Tüm işlere bakarak ata',
   'Bugünün işlerine bakarak ata',
   'Özel bir tarihe bakarak ata',
   'Birime eklenen son kullanıcının eklenme tarihine bakarak ata',
 ];
+
+const adimHedefTipTarih = ref('2025/09/02');
+const proxyAdimHedefTipTarih = ref('2019/03/01');
+const updateProxyAdimHedefTipTarih = () => {
+  proxyAdimHedefTipTarih.value = adimHedefTipTarih.value;
+};
+const selectAdimHedefTipTarih = () => {
+  adimHedefTipTarih.value = proxyAdimHedefTipTarih.value;
+};
 </script>
