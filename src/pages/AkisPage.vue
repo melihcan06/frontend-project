768e7b7311565cb4509fdd7e98b3d3926b1b6a9e
Melih Can <melihcankilic@gmail.com>
2025-09-02 22:34:17 +0300

    a
    a

--- src/pages/AkisPage.vue
+++ src/pages/AkisPage.vue
@@ -5,108 +5,65 @@
   </div>
 
   <div class="q-pa-md">
-    <div class="cursor-pointer">
-      {{ label }}
-      <q-popup-edit v-model="label" auto-save v-slot="scope">
-        <q-select
-          standout
-          :options="optionsAkisAdimTip"
-          label="Adım Türü"
-          v-model="scope.value"
-          dense
-          autofocus
-          counter
-          @keyup.enter="scope.set"
-        />
-        <q-select
-          standout
-          v-model="akisAdimTip"
-          :options="optionsAkisAdimTip"
-          label="Adım Türü"
-        />
-        <q-select
-          standout
-          v-model="atanacakKullanicilar"
-          :options="optionsAtanacakKullanicilar"
-          label="Kişi / Kişiler"
-          multiple
-          behavior="dialog"
-        />
-        <q-select
-          standout
-          v-model="akisAdimHedefTip"
-          :options="optionsAkisAdimHedefTip"
-          label="İş Atama Şekli"
-        />
-      </q-popup-edit>
-    </div>
-  </div>
+    <q-btn push color="primary" :label="adimLabel">
+      <q-popup-proxy>
+        <q-banner>
+          İş Akış Adım
+          <q-select
+            standout
+            v-model="akisAdimTip"
+            :options="optionsAkisAdimTip"
+            label="Adım Türü"
+          />
+          <q-select
+            standout
+            v-model="atanacakKullanicilar"
+            :options="optionsAtanacakKullanicilar"
+            label="Kişi / Kişiler"
+            multiple
+            behavior="dialog"
+          />
+          <q-select
+            standout
+            v-model="akisAdimHedefTip"
+            :options="optionsAkisAdimHedefTip"
+            label="İş Atama Şekli"
+          />
+          <div class="q-pa-md">
+            <div class="q-mb-sm">
+              <q-badge color="teal"> Tarih: {{ adimHedefTipTarih }} </q-badge>
+            </div>
 
-  <div class="q-pa-md" style="max-width: 300px">
-    <div class="q-gutter-md">
-      <q-select
-        standout
-        v-model="akisAdimTip"
-        :options="optionsAkisAdimTip"
-        label="Adım Türü"
-      />
-      <q-select
-        standout
-        v-model="atanacakKullanicilar"
-        :options="optionsAtanacakKullanicilar"
-        label="Kişi / Kişiler"
-        multiple
-        behavior="dialog"
-      />
-      <!-- <q-input v-model="kisiHavuzu" type="text" label="Kişi Havuzu Kod ile" /> -->
-      <q-select
-        standout
-        v-model="akisAdimHedefTip"
-        :options="optionsAkisAdimHedefTip"
-        label="İş Atama Şekli"
-      />
-    </div>
-  </div>
-
-  <!-- <div class="q-pa-md">
-    <div class="q-gutter-md row items-start">
-      <q-date v-model="date" minimal today-btn />
-    </div>
-  </div> -->
-
-  <div class="q-pa-md">
-    <div class="q-mb-sm">
-      <q-badge color="teal"> Tarih: {{ adimHedefTipTarih }} </q-badge>
-    </div>
-
-    <q-btn icon="event" round color="primary">
-      <q-popup-proxy
-        @before-show="updateProxyAdimHedefTipTarih"
-        cover
-        transition-show="scale"
-        transition-hide="scale"
-      >
-        <q-date v-model="proxyAdimHedefTipTarih">
-          <div class="row items-center justify-end q-gutter-sm">
-            <q-btn label="Cancel" color="primary" flat v-close-popup />
-            <q-btn
-              label="OK"
-              color="primary"
-              flat
-              @click="selectAdimHedefTipTarih"
-              v-close-popup
-            />
+            <q-btn icon="event" round color="primary">
+              <q-popup-proxy
+                @before-show="updateProxyAdimHedefTipTarih"
+                cover
+                transition-show="scale"
+                transition-hide="scale"
+              >
+                <q-date v-model="proxyAdimHedefTipTarih">
+                  <div class="row items-center justify-end q-gutter-sm">
+                    <q-btn label="Cancel" color="primary" flat v-close-popup />
+                    <q-btn
+                      label="OK"
+                      color="primary"
+                      flat
+                      @click="selectAdimHedefTipTarih"
+                      v-close-popup
+                    />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-btn>
           </div>
-        </q-date>
+        </q-banner>
       </q-popup-proxy>
     </q-btn>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const label = ref('Aç');
+import { ref, watch } from 'vue';
 
 // const kisiHavuzu = ref(null);
 const akisAdimTip = ref(null);
@@ -128,7 +85,7 @@ const selectAdimHedefTipTarih = () => {
   adimHedefTipTarih.value = proxyAdimHedefTipTarih.value;
 };
 
-const atanacakKullanicilar = ref(null);
+const atanacakKullanicilar = ref<string[]>();
 const optionsAtanacakKullanicilar = [
   'Ahmet Talha Çankal',
   'Erkan Feyyaz Yerdurmaz',
@@ -136,4 +93,39 @@ const optionsAtanacakKullanicilar = [
   'Mehmet Talha Kara',
   'Melih Can Kılıç',
 ];
+
+const adimLabel = ref('Yeni Adım');
+const getAdimLabel = () => {
+  if (akisAdimTip.value === 'Kişi') {
+    if (
+      atanacakKullanicilar.value != null && //libutil list is not empty fonk. yap!
+      atanacakKullanicilar.value[0] != undefined
+    ) {
+      const totalComplete = ref('');
+      atanacakKullanicilar.value.forEach((el, idx) => {
+        if (idx < 2) {
+          totalComplete.value += el + ', ';
+        }
+      });
+      return totalComplete.value;
+    } else {
+      return 'Kişi Seçilmemiş';
+    }
+  } else {
+    return 'Birim veya Rol';
+  }
+};
+watch(akisAdimTip, (eskiAdimTip, yeniAdimTip) => {
+  console.log('watch akisAdimTip');
+  console.log(eskiAdimTip, yeniAdimTip);
+  adimLabel.value = getAdimLabel();
+});
+watch(
+  atanacakKullanicilar,
+  (eskiAtanacakKullanicilar, yeniAtanacakKullanicilar) => {
+    console.log('watch atanacakKullanicilar');
+    console.log(eskiAtanacakKullanicilar, yeniAtanacakKullanicilar);
+    adimLabel.value = getAdimLabel();
+  }
+);
 </script>
